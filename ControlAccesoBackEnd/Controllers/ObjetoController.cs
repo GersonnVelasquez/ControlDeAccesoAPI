c496df7e9f19ac69c6cc28719fbc92f2ca257528
Naomy Rios <zoey.rios@unitec.edu>
2019-02-25 12:19:47 -0600

    Controladores N+R
    Probados en Postman los controladores relacionados a visita correspondientes a mi, Objeto + Persona corresponden a Reina ella los hizo solo falta que los pruebe

---
+++ ControlAccesoBackEnd/Controllers/ObjetoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ControlAccesoBackEnd.Controllers
+{
+    public class ObjetoController : ApiController
+    {
+        Data DB = new Data();
+
+        [HttpGet]
+        [Route("api/Objeto/select/")]
+        public IHttpActionResult Get()
+        {
+            return Ok(DB.Objeto.ToList());
+        }
+
+
+        [HttpPost]
+        [Route("api/Objeto/insert/")]
+        public IHttpActionResult post(Objeto n)
+        {
+            DB.Objeto.Add(n);
+            DB.SaveChanges();
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("api/Objeto/update/")]
+        public IHttpActionResult Update(Objeto n)
+        {
+
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        [Route("api/Objeto/delete/{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            DB.Objeto.Remove(DB.Objeto.Find(id));
+            DB.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ ControlAccesoBackEnd/Controllers/PersonaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ControlAccesoBackEnd.Controllers
+{
+    public class PersonaController : ApiController
+    {
+        Data DB = new Data();
+
+        [HttpGet]
+        [Route("api/Persona/select/")]
+        public IHttpActionResult Get()
+        {
+            return Ok(DB.Persona.ToList());
+        }
+
+
+        [HttpPost]
+        [Route("api/Persona/insert/")]
+        public IHttpActionResult post(Persona n)
+        {
+            DB.Persona.Add(n);
+            DB.SaveChanges();
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("api/Persona/update/")]
+        public IHttpActionResult Update(Persona n)
+        {
+
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        [Route("api/Persona/delete/{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            DB.Persona.Remove(DB.Persona.Find(id));
+            DB.SaveChanges();
+            return Ok();
+        }
+    }
+}
